dbe32daca8c4a9a0393fb1d90a6e9c64a029f834
koba1108 <paseli.koba@gmail.com>
2023-10-24 18:04:02 +0900

    add

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,15 @@
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 import {initializeApp} from 'firebase-admin/app';
 import {getAuth} from 'firebase-admin/auth';
 
 initializeApp();
 
-export const helloWorld = onRequest((req, res) => {
-  logger.info("Hello logs!", {structuredData: true});
-  res.send("Hello from Firebase!");
-});
-
 export const createUser = onRequest(async (req, res) => {
   const auth = getAuth().tenantManager().authForTenant('tenant1-k95n3')
   const userRecord = await auth.createUser({
-    email: 'paseli.koba@gmail.com',
+    email: 'xxxxxxxx@example.com',
     password: 'password',
+    emailVerified: false, // メアド確認不要ならtrue
   })
   res.send(userRecord.toJSON());
 });
